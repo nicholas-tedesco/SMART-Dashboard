47b7f227fd46b3c64f0c8edbf07716bcef381e31
nicktedesco <tedesco1999@gmail.com>
2023-03-29 21:02:29 -0400

    setup

---
+++ app.R
@@ -0,0 +1,35 @@
+## README ----
+
+# app.R
+# clinical trial dashboard
+
+# init NT 03/29
+# updated NT 03/29
+
+
+
+
+## packages ----
+
+library(shiny)
+
+
+
+
+## ui ----
+
+ui <- fluidPage()
+
+
+
+
+## server ----
+
+server <- function(input, output) {}
+
+
+
+
+## run application ----
+
+shinyApp(ui = ui, server = server)

909cb6223685b5d5a75ed83011fc86b77bd9fc5e
nicktedesco <tedesco1999@gmail.com>
2023-04-03 09:16:37 -0400

    treatment randomization, no failure logic

--- app.R
+++ app.R
@@ -24,15 +24,23 @@ library(shinythemes)
 
   # treatment assignment
 
-  randomizer <- function(num){
+  randomizer <- function(num, treat1){
     # first treatment
-    if(num == 1){
-      choices <- c('IV methylprednisolone (30mg twice daily)', 'upadacitinib (30mg twice daily)', 'IV methylprednisolone (30mg twice daily) + upadacitinib (45mg daily)')
-      sample(choices, 1)
-    # second treatment
-    } else if(num == 2){
-      choices <- c('IV methylprednisolone (30mg twice daily)', 'upadacitinib (30mg twice daily)', 'IV methylprednisolone (30mg twice daily) + upadacitinib (45mg daily)')
-      sample(choices, 1)
+    if(num == 1) {
+      choices1 <- c('IV methylprednisolone (30mg twice daily)', 'upadacitinib (30mg twice daily)', 'IV methylprednisolone (30mg twice daily) + upadacitinib (45mg daily)')
+      sample(choices1, 1)
+    # second treatment (depends on first treatment)
+    } else {
+      if(treat1 == 'IV methylprednisolone (30mg twice daily)') {
+        choices2 <- c('add oyolosporin rescue', 'add upa 30mg BID rescue')
+        sample(choices2, 1)
+      } else if (treat1 == 'upadacitinib (30mg twice daily)') {
+        choices2 <- c('switch to IV methylprednisolone 30 mg BID + cycloxporine', 'add IV methylprednisolone 30mg BID')
+        sample(choices2, 1)
+      } else {
+        choices2 <- c('increase upa to 30mg BID', 'switch to oyolosporin rescue')
+        sample(choices2, 1)
+      }
     }
   }
 
@@ -79,24 +87,22 @@ library(shinythemes)
   # Page 1: Treatment Assignment and Log
 
   AA_page <- tabPanel(
-  
     # titles 
     title = 'Randomizer',
     titlePanel('Treatment Assignment'),
-  
     # sidebar
     sidebarLayout(
-    
       # randomizer
       sidebarPanel(
         title = 'Inputs',
         textInput(inputId = 'patient_id', label = 'Patient ID'),
         selectInput('treatment_num', 'Treatment Number', choices = c('Not Selected' = 0, 'First' = 1, 'Second' = 2)),
         actionButton('submit', 'Submit')),
-    
-      # show current log
+      # show current data
       mainPanel(
         DT::dataTableOutput('responses')
+      # error message
+      
       )
     )
   )
@@ -126,29 +132,33 @@ ui <- navbarPage('Clinical Trial Dashboard',
 
 server <- function(input, output, session) {
   
-  # randomizer
+  # randomizer (tab 1)
   
-  id <- eventReactive(input$submit, input$patient_id)
-  num <- eventReactive(input$submit, input$treatment_num)
+    # input from randomizer
+    id <- eventReactive(input$submit, input$patient_id)
+    num <- eventReactive(input$submit, input$treatment_num)
     
-  output$responses <- DT::renderDataTable(random_to_df(id(), num(), randomizer(num())))  
+    # generate data
+    formData <- reactive({
+      random_to_df(id(), num(), randomizer(num(), treat1()))
+    })
   
-  formData <- reactive({
-    data <- random_to_df(id(), num(), randomizer(num()))
-    data
-  })
-  
-  # When the Submit button is clicked, save the form data
-  observeEvent(input$submit, {
-    saveData(formData())
-  })
+    # save data upon submit click
+    observeEvent(input$submit, {
+      saveData(formData())
+    })
     
-  # Show the previous responses
-  # (update with current response when Submit is clicked)
-  output$responses <- DT::renderDataTable({
-    input$submit
-    loadData()
-  })    
+    # Show the previous responses, updated with current
+    output$responses <- DT::renderDataTable({
+      input$submit
+      loadData()
+    })
+    
+    # keep track of treatment 1 for given patient 
+    treat1 <- eventReactive(
+      input$submit, 
+      data.frame(loadData()) %>% filter(patient_id == id()) %>% select(assignment)
+    )
  
 }
 
